108b49ba3c30f37c9f3004737260e0c30b7a477f
FDD13 <122601072+FDD13@users.noreply.github.com>
2024-08-31 16:46:18 +0500

    Вынес скачивание картинок наружу

--- fetch_image_helpers.py
+++ fetch_image_helpers.py
@@ -4,19 +4,11 @@ import os
 from urllib.parse import urlparse
 
 
-
-
-def get_pictures(urls: list, images_path: str):
-    os.makedirs(images_path, exist_ok=True)
-    for image_number, url in enumerate(urls):
-        response = requests.get(url)
-        response.raise_for_status()
-        splitted_url = urllib.parse.urlsplit(url)
-        file_name = os.path.basename(url)
-        file_extension = os.path.splitext(splitted_url.path)
-        with open(os.path.join(images_path, f'image_{image_number}.{file_extension[1]}'), 'wb') as file:
-            file.write(response.content)
-            file.close()
+def download_img(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    file_extension = os.path.splitext(url)
+    return response, file_extension[1]
 
 
 
