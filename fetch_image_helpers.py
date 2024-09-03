5a2d8cc0cfb47bf5f8b6a4d48917ba27a0a51c56
FDD13 <122601072+FDD13@users.noreply.github.com>
2024-09-03 13:21:40 +0500

    Перенес запись файла

--- fetch_image_helpers.py
+++ fetch_image_helpers.py
@@ -4,11 +4,14 @@ import os
 from urllib.parse import urlparse
 
 
-def download_img(url):
+def download_img(url, images_path, image_number):
+    filename = f'image_{image_number}'
     response = requests.get(url)
     response.raise_for_status()
     file_extension = os.path.splitext(url)
-    return response, file_extension[1]
+    with open(os.path.join(images_path, f"{filename}{file_extension[1]}"), 'wb') as file:
+        file.write(response.content)
+
 
 
 
