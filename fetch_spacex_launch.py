8873e20d0149af19763d2c66b5c6591c9296e95f
FDD13 <122601072+FDD13@users.noreply.github.com>
2024-09-03 13:18:39 +0500

    Убрал слова-паразиты с названия перемнной
    Перенес запись файла в вспомогательную функцию
    Убрал неиспользуемый код

--- fetch_spacex_launch.py
+++ fetch_spacex_launch.py
@@ -6,14 +6,14 @@ import argparse
 from dotenv import load_dotenv
 from pathlib import Path
 from urllib.parse import urlparse, urlsplit
-from fetch_image_helpers import get_pictures, download_img
+from fetch_image_helpers import download_img
 
 
 def get_spacex_urls(url, id: str):
     spacex_response = requests.get(url)
     spacex_response.raise_for_status()
-    spacex_image_data = spacex_response.json()
-    spacex_urls = spacex_image_data['links']['flickr']['original']
+    spacex_image_packet = spacex_response.json()
+    spacex_urls = spacex_image_packet['links']['flickr']['original']
     return spacex_urls
 
 
@@ -26,8 +26,6 @@ def main():
     args = parser.parse_args()
 
 
-    load_dotenv()
-    spacex_api_key = os.getenv("NASA_API_KEY")
     spacex_url = f'https://api.spacexdata.com/v5/launches/{args.flight_id}'
     latest_spacex_url = 'https://api.spacexdata.com/v5/launches/latest'
     images_path = 'Space X images'
@@ -37,9 +35,9 @@ def main():
 
 
     for image_number, url in enumerate(spacex_pics_urls):
-        spacex_pic = download_img(url)
-        with open(os.path.join(images_path, f'image_{image_number}{spacex_pic[1]}'), 'wb') as file:
-            file.write(spacex_pic[0].content)
+        spacex_pic = download_img(url, images_path, image_number)
+
+
 
 
 
